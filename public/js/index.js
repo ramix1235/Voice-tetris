cc1b39f1f29a56ca1018d7340aef38f431a693fa
Vladyslav Kharitonov <Vladyslav Kharitonov>
2017-11-18 13:57:45 +0200

    updated libs, added lights to the scene, added models import functionality

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,21 @@ function init() {
   addStats();
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 5000);
   camera.position.set(0, 0, 100);
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor(0xFFFFFF);
+  renderer.setClearColor(0xF5F5F5);
   renderer.shadowMap.enabled = true;
   container.appendChild(renderer.domElement);
 
   activeObj = createBox(30, 30, 30, -25, 0, 50);
   create3DText('Vice Tetrice', -5, 0, 65);
 
+  loadFBXModel('../../public/models/base.fbx',1, 1, 1, 55, -180, 0);
+
+  addLights();
   addOrbitControls();
   addEventListeners();
 };
@@ -48,7 +51,7 @@ function speeching() {
   } else if (~command.indexOf('r') || ~command.indexOf('п')) {
     activeObj.position.x += 10;
   };
-}
+};
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -62,7 +65,27 @@ function updateDevModules() {
           devModules[module].update();
       }
   }
-}
+};
+
+function addLights() {
+  const ambientLight = new THREE.AmbientLight(0x404040, 3);
+
+  const spotLight = new THREE.SpotLight(0xffffff);
+  spotLight.position.set(50, 100, 0);
+  spotLight.castShadow = true;
+  spotLight.shadow.mapSize.width = 1024;
+  spotLight.shadow.mapSize.height = 1024;
+
+  const spotLight2 = new THREE.SpotLight(0xffffff);
+  spotLight2.position.set(50, -200, 0);
+  spotLight2.castShadow = true;
+  spotLight2.shadow.mapSize.width = 1024;
+  spotLight2.shadow.mapSize.height = 1024;
+
+  const spotLightHelper = new THREE.SpotLightHelper(spotLight, 0x000000);
+  const spotLightHelper2 = new THREE.SpotLightHelper(spotLight2, 0x000000);
+  scene.add(ambientLight, spotLight, spotLight2, spotLightHelper, spotLightHelper2);
+};
 
 function addOrbitControls() {
   devModules.controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -71,7 +94,7 @@ function addOrbitControls() {
   //devModules.controls.minDistance = 5;
   //devModules.controls.enablePan = false;
   //devModules.controls.noZoom = true;
-}
+};
 
 function addStats() {
 	devModules.stats = new Stats();
@@ -104,4 +127,47 @@ function createBox(sizeX, sizeY, sizeZ, posX, posY, posZ) {
   boxMesh.position.set(posX, posY, posZ);
   scene.add(boxMesh);
   return boxMesh;
-}
+};
+
+function loadFBXModel(path, scaleX, scaleY, scaleZ, posX, posY, posZ) {
+  const manager = new THREE.LoadingManager();
+  manager.onProgress = function (item, loaded, total) {
+    console.log(item, loaded, total);
+  };
+  const onProgress = function (xhr) {
+    if (xhr.lengthComputable) {
+      var percentComplete = xhr.loaded / xhr.total * 100;
+      console.log(Math.round(percentComplete, 2) + '% downloaded');
+    }
+  };
+  const onError = function (xhr) {
+    console.log(xhr);
+  };
+  const loaderFBX = new THREE.FBXLoader(manager);
+  loaderFBX.load('../../public/models/base.fbx', function (object) {
+    object.traverse(function (child) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+    });
+    object.castShadow = true;
+    object.receiveShadow = true;
+    object.scale.set(scaleX, scaleY, scaleZ);
+    object.position.set(posX, posY, posZ);
+    scene.add(object);
+  }, onProgress, onError);
+
+// LOAD OBJ MODEL
+//   var objLoader = new THREE.OBJLoader();
+//   objLoader.setPath('../../public/models/');
+//   objLoader.load( 'Floating Base.obj', function ( obj ) {
+//     obj.traverse(function (child) {
+//       if (child instanceof THREE.Mesh) {
+//         // child.material = material;
+//         // child.material.map = texture;
+//       }
+//     });
+//     obj.castShadow = true;
+//     obj.receiveShadow = true;
+//     scene.add(obj);
+//   }, onProgress, onError);
+};
